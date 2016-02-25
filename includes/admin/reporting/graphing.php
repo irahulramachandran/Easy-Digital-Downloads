2dd27aa2d59bc25e7533a5e2c4ee91d76e13c15c
Chris Klosowski <cklosowski@gmail.com>
2016-02-25 16:13:31 -0700

    Fix day_by_day being incorrectly set on reports #4250

--- includes/admin/reporting/graphing.php
+++ includes/admin/reporting/graphing.php
@@ -35,7 +35,8 @@ function edd_reports_graph() {
 			$day_by_day = false;
 			break;
 		case 'other' :
-			if( $dates['m_end'] - $dates['m_start'] >= 2 || $dates['year_end'] > $dates['year'] && ( $dates['m_start'] != '12' && $dates['m_end'] != '1' ) ) {
+			// ( $dates['m_start'] - $dates['m_end'] ) != 11 ) identifies getting results from December - January
+			if( $dates['m_end'] - $dates['m_start'] >= 2 || ( $dates['year_end'] > $dates['year'] && ( $dates['m_start'] - $dates['m_end'] ) != 11 ) ) {
 				$day_by_day = false;
 			} else {
 				$day_by_day = true;
