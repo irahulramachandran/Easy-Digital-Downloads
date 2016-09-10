25db42f6e0deb9731f1a4556ac5bcdfc15987ea6
venkat <venkatesan.tk@sakhatech.com>
2016-09-10 11:26:08 +0530

    SIL-375 Label Changed

--- templates/widget-cart-item.php
+++ templates/widget-cart-item.php
@@ -75,7 +75,7 @@
 									<div class="modal-header btn-danger">
 									<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
 									</button>
-									<h4 class="modal-title" id="popupPenalitiesLabel">Penalities</h4>
+									<h4 class="modal-title" id="popupPenalitiesLabel">Policies</h4>
 
 									</div>
 									<div class="modal-body">
