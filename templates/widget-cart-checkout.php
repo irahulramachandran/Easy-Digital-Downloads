2c0ff9f6230635e7b06edaeda5fa8c0ce756b904
Venkatesan TK <venkatesan.tk@sakhatech.com>
2017-05-17 15:43:18 +0530

    EDD Value

--- templates/widget-cart-checkout.php
+++ templates/widget-cart-checkout.php
@@ -1,5 +1,5 @@
-<?php 
-	$showerrormessage = edd_show_max_occupancy_message(); 
+<?php
+	$showerrormessage = edd_show_max_occupancy_message();
 	$className ="";
 	if(!$showerrormessage){
 		$className = "hide";
@@ -24,7 +24,13 @@
   <span class="cart-tax pull-right"><?php echo edd_currency_filter( edd_format_amount( edd_ibe_calculate_tax(edd_get_cart_total()) ) ); ?></span>
 </li>
 <?php endif; ?>
+<?php
+ session_start(); 
+ $_SESSION['totval']= edd_currency_filter( edd_format_amount( edd_get_cart_total() ) );
+ $total_amt = edd_currency_filter( edd_format_amount( edd_get_cart_total() ) );
+?>
 <li class="cart_item edd-cart-meta edd_total edd-cart-item col-xs-12"><?php _e( 'Grand Total', 'easy-digital-downloads' ); ?> <span class="cart-total pull-right"><?php echo edd_currency_filter( edd_format_amount( edd_get_cart_total() ) ); ?></span></li>
+
 <li class="cart_item edd_checkout">
   <a href="#" class="btn btn-primary btn-addanotherroom col-xs-6 no-padding"><?php _e( 'ADD ANOTHER ROOM', 'easy-digital-downloads' ); ?></a>
   <a href="<?php echo edd_get_checkout_uri(); ?>"  class="btn btn-danger col-xs-6 no-padding"><?php _e( 'CHECKOUT', 'easy-digital-downloads' ); ?></a>
